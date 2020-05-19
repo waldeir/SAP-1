302be4aeee13f5eb122e8678eb2381049c8ebe57
waldeir <waldeir@yandex.com>
2020-05-19 18:41:15 -0300

    Switching variables to lower case

--- src/sap1.vhd
+++ src/sap1.vhd
@@ -3,11 +3,11 @@ use IEEE.std_logic_1164.ALL;
 
 entity sap1 is
 	port (
-  bar_HLT     : out std_logic;
-  CLK     : in std_logic;
-  bar_CLK : in std_logic;
-  CLR     : in std_logic;
-  bar_CLR : in std_logic;
+  bar_hlt : out std_logic;
+  clk     : in std_logic;
+  bar_clk : in std_logic;
+  clr     : in std_logic;
+  bar_clr : in std_logic;
   sap_out : out std_logic_vector(7 downto 0)
 	);
 end entity sap1;
@@ -21,14 +21,14 @@ architecture structure of sap1 is
  component pc is
 	port (
 	-- The output port to W bus
-	W_bus   : out std_logic_vector ( 3 downto 0);
-	-- Outputs the counter value to W_bus
-	Ep      : in  std_logic;
+	w_bus   : out std_logic_vector ( 3 downto 0);
+	-- Outputs the counter value to w_bus
+	ep      : in  std_logic;
 	-- Increment program counter by 1
-	Cp      : in std_logic;
-	bar_CLK : in std_logic;
+	cp      : in std_logic;
+	bar_clk : in std_logic;
 	-- Reset counter to 0000 asynchronously
-	bar_CLR     : in std_logic
+	bar_clr     : in std_logic
 	);
 end component pc;
  
@@ -36,11 +36,11 @@ component regb is
     
   port (
   -- Read from W bus 
-  bar_Lb   : in std_logic;
+  bar_lb   : in std_logic;
   -- Clock
-  CLK      : in std_logic;
+  clk      : in std_logic;
   -- The input port from W bus 
-  W_bus    : in std_logic_vector(7 downto 0);
+  w_bus    : in std_logic_vector(7 downto 0);
   -- The output to the addsub
   out_to_addsub  : out std_logic_vector(7 downto 0)
     
@@ -54,9 +54,9 @@ component regout is
   -- Read from W bus 
   bar_Lo   : in std_logic;
   -- Clock
-  CLK      : in std_logic;
+  clk      : in std_logic;
   -- The input port from W bus 
-  W_bus    : in std_logic_vector(7 downto 0);
+  w_bus    : in std_logic_vector(7 downto 0);
   -- The output of the sap
   sap_out  : out std_logic_vector(7 downto 0)
   );
@@ -65,12 +65,12 @@ end component regout;
 
 component mar is
   port (
-  -- Enable read from W_bus
-  bar_Lm     : in std_logic;
+  -- Enable read from w_bus
+  bar_lm     : in std_logic;
   -- Clock
-  CLK        : in std_logic; 
+  clk        : in std_logic; 
   -- Input from W bus
-  W_bus      : in std_logic_vector(3 downto 0);
+  w_bus      : in std_logic_vector(3 downto 0);
   -- Output to ram
   out_to_ram : out std_logic_vector(3 downto 0)
   );
@@ -80,17 +80,17 @@ end component mar;
 component ir is
   port (
     -- Enable read from W bus
-    bar_Li  : in std_logic;
+    bar_li  : in std_logic;
     -- Enable write to W bus and controller sequencer
-    bar_Ei   : in std_logic;
+    bar_ei   : in std_logic;
     -- Clock
-    CLK     : in std_logic;
+    clk     : in std_logic;
     -- Clear instruction register
-    CLR     : in std_logic;
+    clr     : in std_logic;
     -- Send to controller sequencer an instruction code
     out_to_conseq: out std_logic_vector(3 downto 0);
     -- Send and receive data from W bus
-    W_bus  : inout std_logic_vector(7 downto 0)
+    w_bus  : inout std_logic_vector(7 downto 0)
   );
 end component ir;
 
@@ -101,12 +101,12 @@ component ctrlseq is
   microinstruction : out std_logic_vector(11 downto 0);
   -- LDA, ADD, SUB, HLT or OUT in binary form
   macroinstruction : in std_logic_vector(3 downto 0);
-  -- CLK
-  CLK              : in std_logic;
-  -- CLR
-  bar_CLR          : in std_logic;
+  -- clk
+  clk              : in std_logic;
+  -- clr
+  bar_clr          : in std_logic;
   -- Halt
-  bar_HLT          : out std_logic
+  bar_hlt          : out std_logic
   );
 end component ctrlseq;
 
@@ -115,13 +115,13 @@ end component ctrlseq;
 component accumulator is
 	port (
 	-- When LOW read from W bus
-	bar_La : in std_logic;
+	bar_la : in std_logic;
 	-- When HIGH write to the W bus
-	Ea     : in std_logic;
+	ea     : in std_logic;
 	-- Clock signal
-	CLK    : in std_logic;
+	clk    : in std_logic;
   -- The input and output port to W bus	
-	W_bus  : inout std_logic_vector (7 downto 0);
+	w_bus  : inout std_logic_vector (7 downto 0);
   -- The output to the addsub
   out_to_addsub: out std_logic_vector (7 downto 0)
 	);
@@ -135,13 +135,13 @@ component addsub is
 	-- Input from B register
 	in_from_bReg     : in std_logic_vector (7 downto 0);
 	-- The output port to the W bus
-	W_bus       : out std_logic_vector(7 downto 0);
+	w_bus       : out std_logic_vector(7 downto 0);
 	-- When high, performs subtraction and sends the results to
 	-- W bus
-	Su          : in std_logic;
+	su          : in std_logic;
 	-- When high, adds accumulator and B register contents and
-	-- sends the results to W_bus
-	Eu          : in std_logic
+	-- sends the results to w_bus
+	eu          : in std_logic
 	);
 end component addsub;
 
@@ -149,11 +149,11 @@ end component addsub;
 component ram is
   port (
     -- Enable read from MAR 
-    bar_CE      : in std_logic;
+    bar_ce      : in std_logic;
     -- Input from MAR
     in_from_mar : in std_logic_vector(3 downto 0 );
     -- Output to W bus
-    W_bus       : out std_logic_vector(7 downto 0 )
+    w_bus       : out std_logic_vector(7 downto 0 )
   );
 end component ram;
 
@@ -162,23 +162,23 @@ end component ram;
 ----------------------------------------
 signal microinstruction : std_logic_vector ( 11 downto 0);
 
--- Cp     : microinstruction(11)    -
--- Ep     : microinstruction(10)    -
--- bar_Lm : microinstruction(9)     -
--- bar_CE : microinstruction(8)     -
--- bar_Li : microinstruction(7)     -
--- bar_Ei : microinstruction(6)     -
--- bar_La : microinstruction(5)     -
--- Ea     : microinstruction(4)     -
--- Su     : microinstruction(3)     -
--- Eu     : microinstruction(2)     -
--- bar_Lb : microinstruction(1)     -
+-- cp     : microinstruction(11)    -
+-- ep     : microinstruction(10)    -
+-- bar_lm : microinstruction(9)     -
+-- bar_ce : microinstruction(8)     -
+-- bar_li : microinstruction(7)     -
+-- bar_ei : microinstruction(6)     -
+-- bar_la : microinstruction(5)     -
+-- ea     : microinstruction(4)     -
+-- su     : microinstruction(3)     -
+-- eu     : microinstruction(2)     -
+-- bar_lb : microinstruction(1)     -
 -- bar_Lo : microinstruction(0)     -
 
 
 
-signal W_bus      : std_logic_vector(7 downto 0);
--- signal bar_CLR    : std_logic;
+signal w_bus      : std_logic_vector(7 downto 0);
+-- signal bar_clr    : std_logic;
 
 signal ram_mar_bus    : std_logic_vector ( 3 downto 0);
 signal acc_addsub_bus : std_logic_vector ( 7 downto 0);
@@ -193,71 +193,71 @@ begin
 
 
 accumulator0: accumulator port map (
-                 bar_La        => microinstruction(5),
-                 Ea            => microinstruction(4),
-                 CLK           => CLK,
-                 W_bus         => W_bus,
+                 bar_la        => microinstruction(5),
+                 ea            => microinstruction(4),
+                 clk           => clk,
+                 w_bus         => w_bus,
                  out_to_addsub => acc_addsub_bus
                  );
 
 addsub0: addsub port map (
-                 Su           => microinstruction(3),
-                 Eu           => microinstruction(2),
-                 W_bus        => W_bus,
+                 su           => microinstruction(3),
+                 eu           => microinstruction(2),
+                 w_bus        => w_bus,
                  in_from_bReg => regb_addsub_bus,
                  in_from_acc  => acc_addsub_bus
                  );
 
 pc0: pc port map (
-                 Cp      => microinstruction(11),
-                 Ep      => microinstruction(10),
-                 W_bus   => W_bus(3 downto 0),
-                 bar_CLK => bar_CLK,
-                 bar_CLR => bar_CLR
+                 cp      => microinstruction(11),
+                 ep      => microinstruction(10),
+                 w_bus   => w_bus(3 downto 0),
+                 bar_clk => bar_clk,
+                 bar_clr => bar_clr
                  );
 
 regb0 : regb port map (
-                 bar_Lb        => microinstruction(1),
-                 W_bus         => W_bus,
-                 CLK           => CLK,
+                 bar_lb        => microinstruction(1),
+                 w_bus         => w_bus,
+                 clk           => clk,
                  out_to_addsub => regb_addsub_bus
                  );
 
 regout0 : regout port map (
                  bar_Lo  => microinstruction(0),
-                 W_bus   => W_bus,
-                 CLK     => CLK,
+                 w_bus   => w_bus,
+                 clk     => clk,
                  sap_out => sap_out
                  );
 
 mar0 : mar port map(
-                 bar_Lm     => microinstruction(9),
-                 W_bus      => W_bus(3 downto 0),
-                 CLK        => CLK,
+                 bar_lm     => microinstruction(9),
+                 w_bus      => w_bus(3 downto 0),
+                 clk        => clk,
                  out_to_ram => ram_mar_bus
                  );
 
 ram0 : ram port map (
-                 bar_CE      => microinstruction(8), 
-                 W_bus       => W_bus,
+                 bar_ce      => microinstruction(8), 
+                 w_bus       => w_bus,
                  in_from_mar => ram_mar_bus
                  );
 
 ir0 : ir port map (
-                 bar_Li        => microinstruction(7),
-                 bar_Ei        => microinstruction(6),
-                 W_bus         => W_bus,
-                 CLK           => CLK,
-                 CLR           => CLR,
+                 bar_li        => microinstruction(7),
+                 bar_ei        => microinstruction(6),
+                 w_bus         => w_bus,
+                 clk           => clk,
+                 clr           => clr,
                  out_to_conseq => ir_ctrlseq_bus
                  );              
 
 ctrlseq0 : ctrlseq port map(
                  microinstruction => microinstruction,
                  macroinstruction => ir_ctrlseq_bus,
-                 CLK              => CLK,
-                 bar_CLR          => bar_CLR,
-                 bar_HLT              => bar_HLT
+                 clk              => clk,
+                 bar_clr          => bar_clr,
+                 bar_hlt              => bar_hlt
                  );
 
 
--- src/sap1_tb.vhd
+++ src/sap1_tb.vhd
@@ -13,25 +13,25 @@ architecture behav of sap1_tb is
 
 component sap1 is
 port (
-  bar_HLT     : out std_logic;
-  CLK     : in std_logic;
-  bar_CLK : in std_logic;
-  CLR     : in std_logic;
-  bar_CLR : in std_logic;
+  bar_hlt     : out std_logic;
+  clk     : in std_logic;
+  bar_clk : in std_logic;
+  clr     : in std_logic;
+  bar_clr : in std_logic;
   sap_out : out std_logic_vector(7 downto 0)
 );
 end component sap1;
 
 -- Intermediate signals
 
-signal CLK : std_logic;
-signal bar_CLK : std_logic;
-signal bar_HLT : std_logic;
+signal clk : std_logic;
+signal bar_clk : std_logic;
+signal bar_hlt : std_logic;
 signal sap_out : std_logic_vector (7 downto 0);
-signal CLR : std_logic := '0';
-signal bar_CLR : std_logic := '1';
+signal clr : std_logic := '0';
+signal bar_clr : std_logic := '1';
 
-constant CLK_period : time := 10 ns;
+constant clk_period : time := 10 ns;
 
 signal stop : std_logic := '0';
 
@@ -40,25 +40,25 @@ signal delayEnable: std_logic := '1';
 begin
 -- Component instantiation
 sap: sap1 port map(
-      CLK     => CLK,
-      bar_CLK => bar_CLK,
-      bar_HLT     => bar_HLT,
+      clk     => clk,
+      bar_clk => bar_clk,
+      bar_hlt     => bar_hlt,
       sap_out => sap_out,
-      CLR     => CLR,
-      bar_CLR => bar_CLR
+      clr     => clr,
+      bar_clr => bar_clr
       );
 
 clk_process : process
 variable simTime: time := 0 ns;
 begin
-  bar_CLK <= '1';
-	CLK <= '0';
-	wait for CLK_period/2;
-  bar_CLK <= '0';
-	CLK <= '1';
-	wait for CLK_period/2;
-  simTime := simTime + CLK_period;
-	if (bar_HLT = '0') then
+  bar_clk <= '1';
+	clk <= '0';
+	wait for clk_period/2;
+  bar_clk <= '0';
+	clk <= '1';
+	wait for clk_period/2;
+  simTime := simTime + clk_period;
+	if (bar_hlt = '0') then
 		stop <= '1';
 		report "Finished";
 		wait;		
@@ -68,15 +68,15 @@ end process clk_process;
 process 
 begin
 	if delayEnable = '1' then
-		CLR <= '1';	
-		bar_CLR <= '0';
+		clr <= '1';	
+		bar_clr <= '0';
 		delayEnable <= '0';
 		report "There";
 		wait for 30 ns;
 	end if; 
 
-	CLR <= '0';
-	bar_CLR <= '1';
+	clr <= '0';
+	bar_clr <= '1';
 	wait for 1 ns;
 
 	if stop = '1' then
